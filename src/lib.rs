a7561a4812d0fe01f3777b0d191c2e9c9ae7a8cb
AndreHordd <157602836+AndreHordd@users.noreply.github.com>
2024-11-20 19:40:28 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
+use pest::Parser;
 use pest_derive::Parser;
+use thiserror::Error;
 
 #[derive(Parser)]
-#[grammar = "./grammar.pest"]
-pub struct Grammar;
+#[grammar = "grammar.pest"]
+pub struct JSONParser;
+
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("Parsing failed: {0}")]
+    PestError(#[from] pest::error::Error<Rule>),
+}
+
+pub type Result<T> = std::result::Result<T, ParseError>;
+
+impl JSONParser {
+    pub fn parse_json(input: &str) -> Result<pest::iterators::Pairs<Rule>> {
+        JSONParser::parse(Rule::JSON, input).map_err(ParseError::from)
+    }
+}
